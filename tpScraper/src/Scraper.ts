127b6d17b623df0782d8b0bbcec49c05cbeb06ed
Jackson Huang <jahuang@tableau.com>
2017-09-22 17:15:54 -0700

    added request/parse

--- tpScraper/src/Scraper.ts
+++ tpScraper/src/Scraper.ts
@@ -1,5 +1,6 @@
 import http = require('http');
 import fs = require('fs');
+import * as WebRequest from 'web-request';
 
 class AuthorRow {
     id: string;
@@ -18,19 +19,32 @@ class WorkbookRow {
     authorId: string;
     imageUrl: string;
     embedUrl: string;
+    viewCount: number;
 }
 
 interface AuthorProfile {
-    workbooks: any[];
+    address: string;
+    name: string;
+    profileName: string;
+    avatarUrl: string;
+    totalNumberOfFollowers: number;
+    totalNumberOfFollowing: number;
+    visibleWorkbookCount: number;
+    workbooks: Workbook[];
 }
 
+interface Workbook {
+    title: string;
+    viewCount: number;
+    defaultViewRepoUrl: string;
+}
 
 class Scraper {
-    private authors: {[key: string]: AuthorRow} = {};
-    private workbooks: {[key: string]: WorkbookRow} = {};
+    private authorsMap: {[key: string]: AuthorRow} = {};
+    private workbooksMap: {[key: string]: WorkbookRow} = {};
     private authorScraped: number = 0;
+    private authorsToQuery: Array<string> = [];
     public constructor(private authorsFile: string, private workbooksFile: string) {
-        console.log("hello");
         fs.appendFileSync(authorsFile, this.GetAuthorTableSchema());
         fs.appendFileSync(workbooksFile, this.GetAuthorTableSchema());
     };
@@ -47,19 +61,68 @@ class Scraper {
             host: "public.tableau.com",
             path: `/profile/api/${authorId}`
         };
+
+        let body: Array<Buffer> = [];
         const req = http.request(options, (res) => {
             console.log(`STATUS: ${res.statusCode}`);
             console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
             res.setEncoding('utf8');
             res.on('data', (chunk) => {
-                let obj: AuthorProfile = JSON.parse(chunk.toString());
-                console.log(obj.workbooks);
+                if (typeof(chunk) === "string") {
+                    this.processAuthorProfile(chunk);
+                } else {
+                    body.push(chunk);
+                }
             });
             res.on('end', () => {
+                this.processAuthorProfile(Buffer.concat(body).toString());
                 console.log('No more data in response.');
             });
         });
-        
+        //req.end();
+
+        let authorRequest = WebRequest.get(`http://public.tableau.com/profile/api/${authorId}`);
+    }
+
+    private processAuthorProfile(profileJSON: string): void {
+        let profile: AuthorProfile = JSON.parse(profileJSON);
+        let workbooks: Array<Workbook> = profile.workbooks;
+        this.processWorkbooks(workbooks, profile.profileName);
+        // this.authorsMap[profile.profileName] = {
+        //     id: profile.profileName,
+        //     name: profile.name,
+        //     followerCount: profile.totalNumberOfFollowers,
+        //     followingCount: profile.totalNumberOfFollowing,
+        //     workbookCount: profile.visibleWorkbookCount,
+        //     avatarUrl: profile.avatarUrl
+        // }
+    }
+
+    private processWorkbooks(workbooks: Array<Workbook>, authorId: string) {
+        workbooks.forEach(wb => {
+            let id: string = wb.title + authorId;
+            if (!this.workbooksMap[id]) {
+                this.workbooksMap[id] = {
+                    id: id,
+                    name: wb.title,
+                    viewCount: wb.viewCount,
+                    imageUrl: this.generateImageUrl(wb.defaultViewRepoUrl),
+                    embedUrl: this.generateEmbedUrl(wb.defaultViewRepoUrl),
+                    authorId: authorId
+                }
+            }
+        });
+    }
+
+    private generateImageUrl(repoUrl: string): string {
+        let parts: Array<string> = repoUrl.split('/');
+        let firstTwo: string = parts[0].substring(0,2);
+        return `https://public.tableau.com/static/images/${firstTwo}/${parts[0]}/${parts[2]}/4_3.png`;
+    }
+
+    private generateEmbedUrl(repoUrl: string): string {
+        let parts: Array<string> = repoUrl.split('/');
+        return `https://public.tableau.com/views/${parts[0]}/${parts[2]}`;
     }
 
     public GetAuthorProfile(authorId: string): void {
@@ -107,7 +170,7 @@ class Scraper {
     }
 
     public GetWorkbookTableSchema(): string {
-        return "id,name,authorId,imageUrl,embedUrl";
+        return "id,name,authorId,imageUrl,embedUrl, viewCount";
     }
 }
 
