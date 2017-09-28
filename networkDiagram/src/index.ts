5c451e83d0ef0478f03f19da7d90b88e9042245d
Jackson Huang <jiangzehuang@gmail.com>
2017-09-28 00:19:55 -0700

    embedded tableau viz

--- networkDiagram/src/index.ts
+++ networkDiagram/src/index.ts
@@ -1,3 +1,26 @@
-document.write("Hello Webpack what's up???????");
+// embed tableau viz
+declare var tableau: any;
 
-document.write("\noh crap it workeeeeed!");
+const url: string = "https://public.tableau.com/shared/F75NCXYXF?:display_count=yes";
+const container: HTMLElement | null = document.getElementById("tableau-viz");
+const options: any = {
+    hideTabs: true,
+    onFirstInteractive: () => {
+        onVizInit();
+    }
+}
+
+let viz:any = new tableau.Viz(container, url, options);
+
+function onVizInit(): void {
+    let dashboard: any = viz.getWorkbook().getActiveSheet();
+    let worksheets: Array<any> = dashboard.getWorksheets();
+    let authorSheet: any = worksheets[0];
+    let workbookSheet: any = worksheets[1];
+    console.log(authorSheet.getName());
+    console.log(workbookSheet.getName());
+}
+
+function getData(): void { 
+    
+}
